b72e03b31823810d7ff112a4afaaaff545bc11a1
leman973 <amalmanjapallil676@gmail.com>
2025-12-25 14:25:19 +0530

    commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,13 @@
 import { ArrowDown } from "lucide-react"
 
 export const HeroSection = () => {
-    return <section id="hero" className="relative min-h-screen pb-20 flex flex-col items-center justify-center px-4 bg-secondary/10">
+    return <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4 bg-secondary/10">
         <div className="md:flex gap-2 justify-center items-center object-contain">
-            <div className="container relative w-[260px] h-60 xl:w-[300px] xl:h-80 md:shrink-0 animate-drift my-8 mx-auto">
+            <div className="hero-title container hero-image relative w-[260px] h-60 xl:w-[300px] xl:h-80 md:shrink-0 animate-drift my-8 mx-auto">
                 <img className="me w-full h-full rounded-2xl animate-floating object-cover origin-center shadow-[0_0_40px_rgba(255,255,255,0.3)]" src="\projects\profile.jpeg" alt="Me" />
 
-                <img className="absolute w-[70px] left-[167px] xl:left-[207px] xl:top-[86%] top-[85%]" src="\projects\rocket.gif" />
-                <img className="absolute w-[70px] left-[-22px] top-[84%] xl:-left-2.5 xl:top-[86%] " src="\projects\rocket.gif" />
+                <img className="absolute rocket rocket-right w-[70px] left-[167px] xl:left-[207px] xl:top-[86%] top-[85%]" src="\projects\rocket.gif" />
+                <img className="absolute rocket rocket-left w-[70px] left-[-22px] top-[84%] xl:-left-2.5 xl:top-[86%] " src="\projects\rocket.gif" />
 
             </div>
             <div className="container max-w-4xl mx-auto text-center z-10 my-3">
@@ -17,13 +17,13 @@ export const HeroSection = () => {
                         <span className="text-primary opacity-0 animate-fade-in-delay-1"> Amal</span>
                         <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Manjapallil</span>
                     </h1>
-                    <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    <p className="hero-text text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
                         I create stellar web experiences with modern technologies.
                         Specializing in front-end development, I build interfaces that are
                         both beautiful and functional.
                     </p>
                     <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-                        <a href="#projects" className="cosmic-button">
+                        <a href="#projects" className="hero-button cosmic-button">
                             View Project
                         </a>
                     </div>
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,29 +5,30 @@ import { IoMoonOutline } from "react-icons/io5";
 import { cn } from "../lib/utils";
 
 
-export const ThemeToggle =()=>{
-    const [isDarkMode, setisDarkMode] = useState(true);
+export const ThemeToggle = () => {
+    const [isDarkMode, setisDarkMode] = useState(() => {
+        const storedTheme = localStorage.getItem("theme");
+        return storedTheme ? storedTheme === "dark" : true; // default DARK
+    });
 
     useEffect(() => {
-        const storedTheme = localStorage.getItem("theme");
-        if(storedTheme=="dark"){
-            setisDarkMode(true);
+        if (isDarkMode) {
             document.documentElement.classList.add("dark");
-        }
-        else{
-            setisDarkMode(false);
+            localStorage.setItem("theme", "dark");
+        } else {
             document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
         }
-    }, [])
-    
+    }, [isDarkMode]);
 
-    const toggleTheme =()=>{
-        if(isDarkMode){
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
             document.documentElement.classList.remove("dark");
             setisDarkMode(false);
             localStorage.setItem("theme", "light");
         }
-        else{
+        else {
             document.documentElement.classList.add("dark");
             setisDarkMode(true);
             localStorage.setItem("theme", "dark");
@@ -37,6 +38,6 @@ export const ThemeToggle =()=>{
     return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-none"
     )}>
-        {isDarkMode ? <IoSunnyOutline className="h-7 w-6 text-yellow-300"/> : <IoMoonOutline className="h-6 w-6 text-blue-900" />}
-        </button>
+        {isDarkMode ? <IoSunnyOutline className="h-7 w-6 text-yellow-300" /> : <IoMoonOutline className="h-6 w-6 text-blue-900" />}
+    </button>
 }
